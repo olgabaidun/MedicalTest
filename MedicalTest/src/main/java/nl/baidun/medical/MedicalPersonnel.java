57ac31a49d5e665f4c78c3179060ebeb58967abe
siarhei <siarheibaidun@gmail.com>
2015-05-02 13:19:03 +0200

    siarheibaidun change2

--- MedicalTest/src/main/java/nl/baidun/medical/MedicalPersonnel.java
+++ MedicalTest/src/main/java/nl/baidun/medical/MedicalPersonnel.java
@@ -5,4 +5,18 @@ public class MedicalPersonnel {
 	 * added by siarheibaidun
 	 */
 	private String address;
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
