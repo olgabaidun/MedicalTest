baa8b70d287a805eb2226cfb6e868f70e8436e18
siarhei <siarheibaidun@gmail.com>
2015-05-02 13:55:21 +0200

    siarheibaidun change3 (after getting olgabaidun change4)

--- MedicalTest/src/main/java/nl/baidun/medical/MedicalPersonnel.java
+++ MedicalTest/src/main/java/nl/baidun/medical/MedicalPersonnel.java
@@ -1,11 +1,22 @@
 package nl.baidun.medical;
 
+/**
+ * This is testclass
+ * 
+ * @author siarheibaidun
+ * @author olgabaidun
+ */
 public class MedicalPersonnel {
 	private String firstname;
 	
 	private String lastname;
 	
 	private String birthdate;
+	
+	/**
+	 * siarehibaidun added grade and setter/getter
+	 */
+	private int grade;
 	
 	/**
 	 * added by siarheibaidun
